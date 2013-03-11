656c50bd053af004295b6d40f44233afbe894a9c
Jason Kölker <jason@koelker.net>
2013-03-10 23:32:53 -0500

    Refactor plotting to be a bit more modular

--- zl/indicators/plot.py
+++ zl/indicators/plot.py
@@ -16,9 +16,12 @@
 
 import pylab
 
+from matplotlib import dates
 from matplotlib import finance
 from matplotlib import transforms
 
+
+# TODO(jkoelker) Skip weekends and non-trading days in plots
 
 def offset(ax, x, y):
     return transforms.offset_copy(ax.transData, x=x, y=y, units='dots')
@@ -34,35 +37,43 @@ def label_bar(ax, label, bar, above=False):
     ax.text(x, y, label, transform=trans)
 
 
+def setup(ax, signal):
+    bars = signal['flip']['bars'] + signal['bars']
+    bars = [[b['dt'].to_pydatetime(), b['open'],
+             b['close'], b['high'], b['low']] for b in bars]
+
+    finance.plot_day_summary(ax, bars)
+
+    flip_end = len(signal['flip']['bars']) - 1
+    label_bar(ax, 'X', bars[0], True)
+    label_bar(ax, 'Y', bars[1], True)
+    label_bar(ax, "X'", bars[flip_end - 1], True,)
+    label_bar(ax, "Y'", bars[flip_end], True)
+
+    for i, bar in enumerate(bars[flip_end:], 1):
+        label_bar(ax, i, bar)
+
+    ax.autoscale_view()
+    return bars
+
+
 def plot_setup(signal, date_strfmt='%b-%d'):
+    weekdays = dates.WeekdayLocator(dates.WEEKDAYS[:-2])
+    day_formatter = dates.DateFormatter(date_strfmt)
+
     fig = pylab.figure(figsize=(16, 9))
     fig.subplots_adjust(bottom=0.2)
 
     ax = fig.add_subplot(111)
     ax.margins(0.05, 0.05)
+    ax.xaxis.set_major_locator(weekdays)
+    ax.xaxis.set_major_formatter(day_formatter)
 
-    bars = signal['flip']['bars'] + signal['bars']
-    bars = [[b['dt'].to_pydatetime(), b['open'],
-             b['close'], b['high'], b['low']] for b in bars]
+    setup(ax, signal)
 
-    ax.set_xticks(range(0, len(bars)))
-    ax.set_xticklabels([bars[i][0].strftime(date_strfmt)
-                        for i in ax.get_xticks()])
-
-    plot_bars = [[i] + bar[1:] for i, bar in enumerate(bars)]
-
-    finance.plot_day_summary(ax, plot_bars)
-
-    flip_end = len(signal['flip']['bars']) - 1
-    label_bar(ax, 'X', plot_bars[0], True)
-    label_bar(ax, 'Y', plot_bars[1], True)
-    label_bar(ax, "X'", plot_bars[flip_end - 1], True,)
-    label_bar(ax, "Y'", plot_bars[flip_end], True)
-
-    for i, bar in enumerate(plot_bars[flip_end:], 1):
-        label_bar(ax, i, bar)
-
+    ax.xaxis_date()
     ax.autoscale_view()
+
     pylab.setp(pylab.gca().get_xticklabels(), rotation=45,
                horizontalalignment='right')
     pylab.show()
